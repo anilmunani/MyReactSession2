61ef255864f8142581eb609672dbd257510c48d6
pruthvi <pkpruthvipatel@gmail.com>
2023-05-31 15:12:11 +0530

    header feature added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import First from '../components/First';
+import Header from '../ComponentP1/Header';
 
 function App() {
   return (
     <div className="App">
      <First/>
+     <Header/>
     </div>
   );
 }
