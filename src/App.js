8875cd9a6d163e9aa69b2e3f88b8b6a1800895f9
isha <ishapb1203@gmail.com>
2023-06-01 10:45:54 +0530

    First Arrow Function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import First from '../components/First';
 import Header from '../ComponentP1/Header';
+import Isha from './componets/isha';
 
 function App() {
   return (
     <div className="App">
      <First/>
      <Header/>
+     <Isha/>
     </div>
   );
 }
---
+++ src/componets/Isha.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Isha = () => {
+  return (
+    <div>Isha</div>
+  )
+}
+export default Isha
