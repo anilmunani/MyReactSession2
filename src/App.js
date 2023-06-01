2f5356a6ee56bea2acfbe99dbf834fdc4cda42c4
dhruv <dkash2811@gmail.com>
2023-06-01 10:19:03 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import First from '../components/First';
 import Header from '../ComponentP1/Header';
+import Hello from './dhruv/Hello';
 
 function App() {
   return (
     <div className="App">
      <First/>
      <Header/>
+     <Hello/>
     </div>
   );
 }
